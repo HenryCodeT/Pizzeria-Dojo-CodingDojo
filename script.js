6fe9c84b2aa5f1dfc8cb260984117a176631346f
HenryCodeT <120686@unsaac.edu.pe>
2022-01-08 21:12:41 -0500

    script

---
+++ script.js
@@ -0,0 +1,81 @@
+// var sandwich = {
+//     pan:    "masa madre",
+//     proteína:  "asado",
+//     queso:   "queso suizo lacey",
+//     salsas: ["lechuga romana", "tomates reliquia", "salsa de rábano"]
+// };
+    
+// console.log(sandwich);
+
+// function sandwichFactory(pan, proteína, queso, salsas) {
+//     var sandwich = {};
+//     sandwich.pan = pan;
+//     sandwich.proteína = proteína;
+//     sandwich.queso = queso;
+//     sandwich.salsas = salsas;
+//     return sandwich;
+// }
+
+// var s1 = sandwichFactory("trigo", "pavo", "provolone", ["mostaza", "cebolla frita", "rúcula"]);
+// console.log(s1);
+
+let pizza = {
+    tipoCorteza:null,
+    tipoSalsa:null,
+    quesos:null,
+    salsas:null,
+}
+console.log(pizza);
+
+function pizzaOven(tipoCorteza,tipoSalsa,quesos,salsas) {
+    let pizza = {}
+    pizza.tipoCorteza=tipoCorteza;
+    pizza.tipoSalsa=tipoSalsa;
+    pizza.quesos=quesos;
+    pizza.salsas=salsas;
+ 
+    return pizza
+}
+let pizza1 = pizzaOven("estilo Chicago","tradicional",["mozzarella"],["pepperoni","salchicha"])
+let pizza2 = pizzaOven("lanzada a mano" , "marinara" , ["mozzarella", "feta"], ["champiñones", "aceitunas", "cebollas"])
+let pizza3 = pizzaOven("pan" , "ketchup" , ["mozzarella", "parmesano"], ["champiñones", "piña"])
+let pizza4 = pizzaOven("pan" , "marinara" , ["mozzarella", "chedar"], ["champiñones", "cabanosi", "cebollas"])
+console.log(pizza1); 
+console.log(pizza2); 
+console.log(pizza3); 
+console.log(pizza4); 
+
+// pizza random
+
+const pizzaRandom =function () {
+    let pizza = {
+        tipoCorteza:null,
+        tipoSalsa:null,
+        quesos:null,
+        salsas:null
+    }
+    let tipoCorteza=["estilo Chicago","lanzada a mano","pan"],
+        tipoSalsa=["tradicional","marinara","ketchup"],
+        quesos=[["mozzarella"],["mozzarella", "feta"],["mozzarella", "parmesano"],["mozzarella", "chedar"]],
+        salsas=[["pepperoni","salchicha"],["champiñones", "aceitunas", "cebollas"],["champiñones", "piña"],["champiñones", "cabanosi", "cebollas"]];
+    
+    let randomTipoCorteza=Math.round(Math.random()*2), 
+        randomTipoSalsa=Math.round(Math.random()*2), 
+        randomQuesos=Math.round(Math.random()*3), 
+        randomSalsas=Math.round(Math.random()*3); 
+
+    console.log(randomTipoCorteza) 
+    console.log(randomTipoSalsa)
+    console.log(randomQuesos)
+    console.log(randomSalsas)
+
+    pizza.tipoCorteza=tipoCorteza[randomTipoCorteza];
+    pizza.tipoSalsa=tipoSalsa[randomTipoSalsa];
+    pizza.quesos=quesos[randomQuesos];
+    pizza.salsas=salsas[randomSalsas];
+
+    
+    return pizza
+}
+
+console.log("Pizza random",pizzaRandom()); 
